18eb051195dc56d2ab54ee4680bf5c8c13131cc7
Fuzzik <fuzzik543@gmail.com>
2014-03-16 15:55:28 -0400

    Documentation / Linux Fixes
    *Changed VGUI to vgui in material files
    *Updated documentation in force_download.lua

--- lua/autorun/server/force_download.lua
+++ lua/autorun/server/force_download.lua
@@ -1,5 +1,5 @@
-// This file will make sure that your clients download the addon materials.
-// Make sure that "materials/vgui/ttt" and "sound/weapons/det_revolver" is uploaded to your FastDL server.
+// This optional file will make sure that your clients download the addon materials.
+// Make sure that "materials/vgui/ttt" and "sound/weapons/det_revolver" is uploaded to your FastDL / FTP server.
 
 -- The Files
 resource.AddFile( "materials/vgui/ttt/icon_mp5.vmt" )
