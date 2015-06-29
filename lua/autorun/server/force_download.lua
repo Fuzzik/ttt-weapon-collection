bae678d8b80cb79f6b5e2e297bc3fd7773bfbfaf
Fuzzik <fuzzik543@gmail.com>
2015-06-29 14:54:32 -0400

    Comment consistency
    I'm picky :P

--- lua/autorun/server/force_download.lua
+++ lua/autorun/server/force_download.lua
@@ -1,4 +1,4 @@
-// This forces clients to download the assets for the weapons.
-// This includes kill icons and a few sounds.
+-- This forces clients to download the assets for the weapons.
+-- This includes kill icons and a few sounds.
 
 resource.AddWorkshop( "194965598" )
