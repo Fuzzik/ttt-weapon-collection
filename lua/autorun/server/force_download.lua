adc734cc0182a015c8d275d92bde35cb8d293fa2
Fuzzik <Fuzzik@users.noreply.github.com>
2019-05-20 14:55:23 -0400

    Clarify force_download.lua comments
    Removed line about FastDL. There is no reason to do that as of right now, and people aren't going to decompile the addon and do that.

--- lua/autorun/server/force_download.lua
+++ lua/autorun/server/force_download.lua
@@ -1,4 +1,3 @@
--- This file forces clients to download the assets for the weapons (weapons icons and sounds).
--- If you are distributing those files via FastDL, comment out the line below.
+-- This file forces clients to download the addon upon connecting for required weapon icons and sounds.
 
 resource.AddWorkshop( "194965598" )
