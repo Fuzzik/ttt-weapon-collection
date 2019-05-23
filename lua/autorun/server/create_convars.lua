32692abc4c2c3e50d9e5acaf796be24b7643d938
Fuzzik <Fuzzik@users.noreply.github.com>
2019-05-23 16:01:28 -0400

    Remove CreateConVar variables
    I forgot to do this when I stopped referencing them in the weapon scripts (the ONLY place they were referenced).

--- lua/autorun/server/create_convars.lua
+++ lua/autorun/server/create_convars.lua
@@ -1,20 +1,20 @@
 -- This file creates convars to allow disabling weapons.
 
-local fuzzik_disable_ak47 = CreateConVar( "fuzzik_disable_ak47", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_aug = CreateConVar( "fuzzik_disable_aug", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_awp = CreateConVar( "fuzzik_disable_awp", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_famas = CreateConVar( "fuzzik_disable_famas", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_frag = CreateConVar( "fuzzik_disable_frag", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_g3sg1 = CreateConVar( "fuzzik_disable_g3sg1", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_galil = CreateConVar( "fuzzik_disable_galil", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_jihad = CreateConVar( "fuzzik_disable_jihad", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_m4a1_s = CreateConVar( "fuzzik_disable_m4a1_s", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_mp5 = CreateConVar( "fuzzik_disable_mp5", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_p90 = CreateConVar( "fuzzik_disable_p90", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_p228 = CreateConVar( "fuzzik_disable_p228", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_usp = CreateConVar( "fuzzik_disable_usp", "0", FCVAR_ARCHIVE ) -- weapon_ttt_pistol
-local fuzzik_disable_pump = CreateConVar( "fuzzik_disable_pump", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_revolver = CreateConVar( "fuzzik_disable_revolver", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_sg552 = CreateConVar( "fuzzik_disable_sg552", "0", FCVAR_ARCHIVE )
-local fuzzik_disable_mp7 = CreateConVar( "fuzzik_disable_mp7", "0", FCVAR_ARCHIVE ) -- weapon_ttt_smg
-local fuzzik_disable_tmp_s = CreateConVar( "fuzzik_disable_tmp_s", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_ak47", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_aug", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_awp", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_famas", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_frag", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_g3sg1", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_galil", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_jihad", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_m4a1_s", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_mp5", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_p90", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_p228", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_usp", "0", FCVAR_ARCHIVE ) -- weapon_ttt_pistol
+CreateConVar( "fuzzik_disable_pump", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_revolver", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_sg552", "0", FCVAR_ARCHIVE )
+CreateConVar( "fuzzik_disable_mp7", "0", FCVAR_ARCHIVE ) -- weapon_ttt_smg
+CreateConVar( "fuzzik_disable_tmp_s", "0", FCVAR_ARCHIVE )
